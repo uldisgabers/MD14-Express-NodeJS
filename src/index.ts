e11619bb01c3a56d7e9d3187acf4369ce25a8cbd
Uldis Gabers <uldisgabers@gmail.com>
2023-12-26 16:43:10 +0200

    commit v3

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { connection } from "./db";
-import express, { Request, Response } from 'express'
-const cors = require('cors');
-var bodyParser = require('body-parser')
+import express, { Request, Response } from "express";
+import { z } from "zod";
+
+const cors = require("cors");
+var bodyParser = require("body-parser");
 const app = express();
 const port = 3001;
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 type Car = {
   id: number;
@@ -17,15 +19,17 @@ type Car = {
   createdAt: string;
 };
 
-app.use(cors({
-  origin: '*'
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-app.get('/cars', async (req, res) => {
+app.get("/cars", async (req, res) => {
   // Execute the query to get all cars
-  connection.query('SELECT * FROM cars', (error, results) => {
+  connection.query("SELECT * FROM cars", (error, results) => {
     if (error) {
-      res.status(500).json({ error: 'Internal Server Error' });
+      res.status(500).json({ error: "Internal Server Error" });
       return;
     }
 
@@ -34,72 +38,97 @@ app.get('/cars', async (req, res) => {
   });
 });
 
-app.post('/car', async (req: Request<{}, {}, Car>, res) => {
+app.post("/car", async (req: Request<{}, {}, Car>, res) => {
   const { brand, color, createdAt, id, img, model, price } = req.body;
-  
-  if (!brand || !color || !createdAt || !img || !model || !price) {
-    res.status(400).send('Invalid data');
+
+  const mySchema = z.string().min(2).max(40);
+
+  const brandValidation = mySchema.safeParse(brand); // => { success: true; data: "tuna" }
+  const modelValidation = mySchema.safeParse(model);
+  const colorValidation = mySchema.safeParse(color);
+  const priceValidation = mySchema.safeParse(price);
+
+  if (
+    !brand ||
+    !color ||
+    !createdAt ||
+    !img ||
+    !model ||
+    !price ||
+    !brandValidation.success ||
+    !modelValidation.success ||
+    !colorValidation.success ||
+    !priceValidation.success
+  ) {
+    res.status(400).send("Invalid data");
     return;
   }
 
-  connection.query(`
+  connection.query(
+    `
     INSERT INTO cars (brand, color, createdAt, img, model, price) 
     VALUES ('${brand}', '${color}', '${createdAt}', '${img}', '${model}', '${price}')
-    `, (error, results) => {
-    if (error) {
-      res.status(500).json({ error: 'Internal Server Error' });
-      return;
+    `,
+    (error, results) => {
+      if (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+        return;
+      }
+
+      res.json({ cars: results });
     }
-
-    res.json({cars: results})
-  })
-})
-
-app.get('/car/:id', async (req, res) => {
-  connection.query(`SELECT * FROM cars WHERE id = ${req.params.id}`, (error, results) => {
-    if (error) {
-      res.status(500).json({ error: 'Internal Server Error' });
-      return;
-    }
-
-    res.json({ data: results });
-  });
+  );
 });
 
-app.delete('/cars/:id', async (req, res) => {
+app.get("/car/:id", async (req, res) => {
+  connection.query(
+    `SELECT * FROM cars WHERE id = ${req.params.id}`,
+    (error, results) => {
+      if (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+        return;
+      }
 
-  connection.query(`
+      res.json({ data: results });
+    }
+  );
+});
+
+app.delete("/cars/:id", async (req, res) => {
+  connection.query(
+    `
     DELETE FROM cars 
     WHERE id='${req.params.id}'
-    `, (error, results) => {
-    if (error) {
-      res.status(500).json({ error: 'Internal Server Error' });
-      return;
+    `,
+    (error, results) => {
+      if (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+        return;
+      }
+
+      res.json("Data deleted succesfully");
     }
+  );
+});
 
-    res.json('Data deleted succesfully')
-  })
-})
-
-app.put('/cars/:id', async (req, res) => {
+app.put("/cars/:id", async (req, res) => {
   const { brand, color, id, model, price } = req.body;
 
-  console.log(req.params.id)
-  console.log(brand)
-
-
-  connection.query(`
+  connection.query(
+    `
     UPDATE cars SET brand = '${brand}', model = '${model}', color = '${color}', price = '${price}' WHERE id = '${req.params.id}'
-  `, (error, result) => {
-    if (error) {
-      res.status(500).json({ error: 'Internal Server Error' });
-      return;
-    }
+  `,
+    (error, result) => {
+      if (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+        return;
+      }
 
-    console.log(result)
-    res.json('Data updated')
-  })
-})
+      console.log(result);
+      res.json("Data updated");
+    }
+  );
+});
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
